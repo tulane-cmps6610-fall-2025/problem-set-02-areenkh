d03328066ece7e6d0a7df9da3125b1c20836746a
Areen <akhalaila@tulane.edu>
2025-09-15 20:53:27 -0500

    add solution

--- main.py
+++ main.py
@@ -42,20 +42,85 @@ def pad(x,y):
         x = ['0'] + x
         y = ['0'] + y
     return x,y
+
+def add_vec(a_vec, b_vec):
+    a=binary2int(a_vec).decimal_val
+    b=binary2int(b_vec).decimal_val
+    return BinaryNumber(a + b).binary_vec
+
+def sub_vec(a_vec, b_vec):
+    a =binary2int(a_vec).decimal_val
+    b = binary2int(b_vec).decimal_val
+    return BinaryNumber(a - b).binary_vec
     
 def quadratic_multiply(x, y):
-    ### TODO
-    pass
-    ###
+    if x.decimal_val == 0 or y.decimal_val== 0:
+        return ['0']
+    if x.decimal_val< 2 and y.decimal_val < 2:  
+        if (x.decimal_val== 1 and y.decimal_val == 1): 
+            return ['1'] 
+        else: 
+            return ['0']
+    
+    vx, vy = pad(x.binary_vec, y.binary_vec)
+    n = len(vx)
+
+    if n == 1:
+        if vx[0]== '1' and vy[0] =='1':
+            return ['1']
+        else:
+            return ['0']
+        
+
+
+    xl, xr = split_number(vx) 
+    yl, yr = split_number(vy)
+    P1 = quadratic_multiply(xl, yl)
+    P2 = quadratic_multiply(xr, yr)
+    P3 = quadratic_multiply(xl, yr)
+    P4 = quadratic_multiply(xr, yl)
+    term1 = bit_shift(binary2int(P1),n).binary_vec
+    cross = add_vec(P3, P4)
+    term2 = bit_shift(binary2int(cross), n //2).binary_vec
+    s = add_vec(term1, term2)
+    s = add_vec(s, P2)
+    return s
+
 
 def subquadratic_multiply(x, y):
-    ### TODO
-    pass
-    ###
+    if x.decimal_val == 0 or y.decimal_val== 0:
+        return ['0']
+    if x.decimal_val<2 and y.decimal_val < 2:  
+        if (x.decimal_val== 1 and y.decimal_val == 1): 
+            return ['1'] 
+        else: 
+            return ['0']
+    vx, vy = pad(x.binary_vec, y.binary_vec)
+    n = len(vx)
+    if n == 1:
+        if vx[0]== '1' and vy[0] =='1':
+            return ['1']
+        else:
+            return ['0']
+    xl, xr = split_number(vx)
+    yl, yr = split_number(vy)
+    P1 = subquadratic_multiply(xl, yl)
+    P2 = subquadratic_multiply(xr, yr)
+    sum_x = add_vec(xl.binary_vec, xr.binary_vec)
+    sum_y = add_vec(yl.binary_vec, yr.binary_vec)
+    P3 = subquadratic_multiply(binary2int(sum_x), binary2int(sum_y))
+    mid = sub_vec(P3, P1)
+    mid = sub_vec(mid, P2)
+    term1 = bit_shift(binary2int(P1), n).binary_vec  
+    term2 = bit_shift(binary2int(mid), n//2).binary_vec
+    s = add_vec(term1, term2)
+    s = add_vec(s, P2)
+    return s
+
 
 ## Feel free to add your own tests here.
 def test_multiply():
-    assert binary2int(quadratic_multiply(BinaryNumber(2), BinaryNumber(2))) == 2*2
+    assert binary2int(quadratic_multiply(BinaryNumber(2), BinaryNumber(2))).decimal_val == 2*2
 
 # some timing functions here that will make comparisons easy    
 def time_multiply(x, y, f):
@@ -81,6 +146,5 @@ def print_results(results):
             headers=['n', 'quadratic', 'subquadratic'],
             floatfmt=".3f",
             tablefmt="github"))
-    
     
 
